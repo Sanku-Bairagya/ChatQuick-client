c79ad32f3653c0051689cedc4b1228325a124e6e
Sanku-Bairagya <sankubairagya911@gmail.com>
2024-12-28 12:59:19 +0530

    Home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import Applayout from '../components/layout/Applayout'
+import React from 'react';
+import Applayout from '../components/layout/Applayout';
 import { Box, Typography } from '@mui/material';
 import { grayColor } from '../constants/color.js';
+import { useMyChatsQuery } from '../redux/api/api.js';
 
 const Home = () => {
+  const { data } = useMyChatsQuery("");
+
   return (
-   <Box bgcolor={grayColor} height={"100%"}>
-    <Typography p={"2rem"} variant='h5' textAlign={"center"} >Select a friend to chat</Typography>
-   </Box>
-  )
-}
+    <Box bgcolor={grayColor} height={"100%"}>
+      {/* Conditional Rendering Based on data.chats */}
+      {data?.chats?.length > 0 ? (
+        <Typography
+          variant="h5"
+          color="textSecondary"
+          textAlign="center"
+          p={"2rem"}
+        >
+          Select friends to chat.
+        </Typography>
+      ) : (
+        <Typography
+          variant="h5"
+          color="textSecondary"
+          textAlign="center"
+          p={"2rem"}
+        >
+          No friends to chat.
+        </Typography>
+      )}
+    </Box>
+  );
+};
 
 export default Applayout()(Home);
